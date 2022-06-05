28f5ac2cfad22a50f81c4acf6bf6600c3b27df73
Maria Sena <96543300+MariasenaS@users.noreply.github.com>
2022-06-04 23:59:01 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// ***Primeiro programa JS***
+// criar um programa para mostrar tipos de linguagem de progração que você esta estudando liste por ordem de prioridade.
+const tipoL = [
+"PHP", "JAVA", "JS" 
+];
+// ao usar cochetes estou criando um array é um objeto
+// imprimindo o array
+console.log(tipoL);
+// listar por ordem de prioridade
+console.log("linguagem de estudo por ordem de prioridade: ", "\n","\n" + tipoL [1],"\n" + tipoL[2], "\n" + tipoL[0]); 
